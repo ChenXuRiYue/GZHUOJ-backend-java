86a4b2d5543f62b8c3cbd2251527695ec9882c62
陈旭明 <chenjiuririyue@outlook.com>
2024-07-09 22:54:16 +0800

    spring1-1

--- gzhuoj-microservices/gzhuoj-user/src/main/java/com/gzhuoj/usr/service/UserService.java
+++ gzhuoj-microservices/gzhuoj-user/src/main/java/com/gzhuoj/usr/service/UserService.java
@@ -1,7 +1,8 @@
 package com.gzhuoj.usr.service;
 
+import com.gzhuoj.usr.model.entity.User;
+import org.springframework.stereotype.Service;
 
 public interface UserService {
-
     String test();
 }
