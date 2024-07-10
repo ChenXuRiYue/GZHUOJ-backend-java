efc2bd702c6a11db71ef73a2ee1e126153375fea
LJCzzzzZ <13893403+lliang-jincheng@user.noreply.gitee.com>
2024-07-10 21:00:46 +0800

    update pom and test userService

--- gzhuoj-microservices/gzhuoj-user/src/main/java/com/gzhuoj/usr/service/Impl/UserServiceImpl.java
+++ gzhuoj-microservices/gzhuoj-user/src/main/java/com/gzhuoj/usr/service/Impl/UserServiceImpl.java
@@ -1,12 +1,29 @@
 package com.gzhuoj.usr.service.Impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.gzhuoj.usr.model.entity.User;
+import com.gzhuoj.usr.model.mapper.UserMapper;
 import com.gzhuoj.usr.service.UserService;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
     @Override
     public String test() {
-        return "  ";
+        User user = User.builder()
+                .email("11")
+                .role(1)
+                .password("111")
+                .updateTime(new Date())
+                .createTime(new Date())
+                .nickname("zzz")
+                .orgnization("kk")
+                .username("ljc")
+                .deleteflag(0)
+                .build();
+        baseMapper.insert(user);
+        return "fuck";
     }
 }
--- gzhuoj-microservices/gzhuoj-user/src/main/java/com/gzhuoj/usr/service/UserService.java
+++ gzhuoj-microservices/gzhuoj-user/src/main/java/com/gzhuoj/usr/service/UserService.java
@@ -1,8 +1,9 @@
 package com.gzhuoj.usr.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.gzhuoj.usr.model.entity.User;
 import org.springframework.stereotype.Service;
 
-public interface UserService {
+public interface UserService extends IService<User> {
     String test();
 }
