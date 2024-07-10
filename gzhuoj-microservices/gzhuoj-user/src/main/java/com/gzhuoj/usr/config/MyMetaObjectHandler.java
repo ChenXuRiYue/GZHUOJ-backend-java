d153b15ac6f95ff9aa8ec796aceab3e86c7f00a5
梁金成 <13893403+lliang-jincheng@user.noreply.gitee.com>
2024-07-10 22:08:18 +0800

    基础配置， 全局拦截器， 返回码，响应码等common组件。

---
+++ gzhuoj-microservices/gzhuoj-user/src/main/java/com/gzhuoj/usr/config/MyMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package com.gzhuoj.usr.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("插入时填充----");
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("更新时填充----");
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+}
