e3d1610cdf4f3451fe86d638f59da6a0f4c65df3
Axel.Xun <chenjiuririyue@outlook.com>
2024-07-11 21:45:53 +0800

    完善题库第一二阶段必要的实体类、依赖等

--- gzhuoj-microservices/gzhuoj-problem/src/main/java/com/gzhuoj/problem/model/entity/TestCase.java
+++ gzhuoj-microservices/gzhuoj-problem/src/main/java/com/gzhuoj/problem/model/entity/TestCase.java
@@ -1,18 +1,22 @@
 package com.gzhuoj.problem.model.entity;
 
+import common.database.BaseDO;
 import lombok.Data;
 
 @Data
-public class TestCase {
-    private String name;
+public class TestCase extends BaseDO {
+    /**
+     * 数据在对应数据集中的相对序号
+     */
+    private Integer testCaseNum;
 
     /**
-     * 题目题面
+     * 输入
      */
-    private String description;
+    private String input;
 
     /**
-     * 题目参考代码
+     * 输出
      */
-    private String soluction;
+    private String output;
 }
