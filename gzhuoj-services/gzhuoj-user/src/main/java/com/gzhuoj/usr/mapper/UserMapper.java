9373a72cc0643081f20e6cd573477e505f5c8e62
Axel.Xun <chenjiuririyue@outlook.com>
2024-07-14 22:54:34 +0800

    fix: 修复了合并之后的一些bug

--- gzhuoj-services/gzhuoj-user/src/main/java/com/gzhuoj/usr/mapper/UserMapper.java
+++ gzhuoj-services/gzhuoj-user/src/main/java/com/gzhuoj/usr/mapper/UserMapper.java
@@ -1,8 +1,13 @@
 package com.gzhuoj.usr.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.gzhuoj.usr.dto.req.AdminPrivilegeListReqDTO;
 import com.gzhuoj.usr.model.entity.UserDO;
 
 
 public interface UserMapper extends BaseMapper<UserDO> {
+//    IPage<UserDO> pageUser(AdminUserListReqDTO requestParam);
+
+    IPage<UserDO> pageUserPrivilege(AdminPrivilegeListReqDTO requestParam);
 }
