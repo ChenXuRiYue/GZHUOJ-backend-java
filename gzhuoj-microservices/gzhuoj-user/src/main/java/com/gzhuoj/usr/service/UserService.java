25badcc2700b73d73df9de615608e828c7fd3de5
梁金成 <13893403+lliang-jincheng@user.noreply.gitee.com>
2024-07-14 21:43:46 +0800

    feat: add 用户登录

--- gzhuoj-microservices/gzhuoj-user/src/main/java/com/gzhuoj/usr/service/UserService.java
+++ gzhuoj-microservices/gzhuoj-user/src/main/java/com/gzhuoj/usr/service/UserService.java
@@ -1,8 +1,18 @@
 package com.gzhuoj.usr.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.gzhuoj.usr.dto.req.UserBatchImportReqDTO;
+import com.gzhuoj.usr.dto.req.UserLoginReqDTO;
+import com.gzhuoj.usr.dto.resp.UserLoginRespDTO;
 import com.gzhuoj.usr.model.entity.UserDO;
+import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
 
 public interface UserService extends IService<UserDO> {
     String test();
+
+    UserLoginRespDTO login(UserLoginReqDTO requestParam);
+
+    ResponseEntity<byte[]> batchImport(UserBatchImportReqDTO requestParam) throws IOException;
 }
