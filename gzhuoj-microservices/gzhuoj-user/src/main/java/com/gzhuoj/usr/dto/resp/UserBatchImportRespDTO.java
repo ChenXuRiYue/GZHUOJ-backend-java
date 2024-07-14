b872cba3c63c34462401b91dd9901dd9ce27d959
LJCzzzzZ <13893403+lliang-jincheng@user.noreply.gitee.com>
2024-07-14 21:44:17 +0800

    feat: add 根据用户名和学号批量导出用户账号和密码

---
+++ gzhuoj-microservices/gzhuoj-user/src/main/java/com/gzhuoj/usr/dto/resp/UserBatchImportRespDTO.java
@@ -0,0 +1,15 @@
+package com.gzhuoj.usr.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserBatchImportRespDTO {
+    public ByteArrayResource file;
+}
