fdc3c07f5b2926ae04584a20b8a31042685ee388
LJCzzzzZ <13893403+lliang-jincheng@user.noreply.gitee.com>
2024-07-20 19:32:12 +0800

    feat: 添加公共配置解决在使用openFeign时Gateway不会自动引入HttpMessageConverters问题

---
+++ gzhuoj-common/src/main/java/common/config/GatewayConfig.java
@@ -0,0 +1,17 @@
+package common.config;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.stream.Collectors;
+
+public class GatewayConfig {
+    @Bean
+    @ConditionalOnMissingBean
+    public HttpMessageConverters messageConverters(ObjectProvider<HttpMessageConverter<?>> converters) {
+        return new HttpMessageConverters(converters.orderedStream().collect(Collectors.toList()));
+    }
+}
