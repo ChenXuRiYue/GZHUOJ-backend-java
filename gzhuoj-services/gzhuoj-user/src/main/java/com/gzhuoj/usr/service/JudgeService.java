2b773e4528c446dae1d6e9027932dc625293b501
LJCzzzzZ <13893403+lliang-jincheng@user.noreply.gitee.com>
2024-07-25 22:06:11 +0800

    feature/1006: 移除部分无用的类

--- gzhuoj-services/gzhuoj-user/src/main/java/com/gzhuoj/usr/service/JudgeService.java
+++
@@ -1,9 +0,0 @@
-package com.gzhuoj.usr.service;
-
-import com.gzhuoj.usr.dto.req.JudgeUploadCaseReqDTO;
-
-import java.util.List;
-
-public interface JudgeService {
-    List<String> uploadJudgeCase(JudgeUploadCaseReqDTO requestParam);
-}
