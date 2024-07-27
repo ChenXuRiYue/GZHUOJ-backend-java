c6e8c952a8fdd3183bf84bd89c899733f665519b
LJCzzzzZ <13893403+lliang-jincheng@user.noreply.gitee.com>
2024-07-27 16:55:56 +0800

    feature/1007: 创建——比赛实体/比赛题目实体/题目气球颜色对应实体

---
+++ gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/mapper/ContestProblemMapper.java
@@ -0,0 +1,7 @@
+package com.gzhuoj.contest.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gzhuoj.contest.model.entity.ContestProblemDO;
+
+public interface ContestProblemMapper extends BaseMapper<ContestProblemDO> {
+}
---
+++ gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/model/entity/ContestProblemDO.java
@@ -0,0 +1,35 @@
+package com.gzhuoj.contest.model.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+@TableName(value = "contest_problem")
+public class ContestProblemDO {
+
+    /**
+     * 比赛编号
+     */
+    private Integer contestId;
+
+    /**
+     * 题目集中的编号
+     */
+    private Integer problemId;
+
+    /**
+     * 题目对应气球颜色rgb十六进制编号
+     */
+    private String problemColor;
+
+    /**
+     * 题目在比赛中的编号
+     */
+    private Integer actualNum;
+}
