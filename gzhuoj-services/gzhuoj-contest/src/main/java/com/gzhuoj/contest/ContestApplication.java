9014b220224fcd4972b819968ee66cc6e0c6927e
LJCzzzzZ <13893403+lliang-jincheng@user.noreply.gitee.com>
2024-07-27 16:55:15 +0800

    feature/1007: 创建比赛接口开发

--- gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/ContestApplication.java
+++ gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/ContestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@MapperScan("com.gzhuoj.contest.mapper")
 @EnableDiscoveryClient
 public class ContestApplication {
     public static void main(String[] args) {
