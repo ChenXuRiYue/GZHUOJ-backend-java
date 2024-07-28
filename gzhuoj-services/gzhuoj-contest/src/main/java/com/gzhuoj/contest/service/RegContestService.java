1fec9bd9100565286efcfe34c074f0ce72ebdf37
LJCzzzzZ <13893403+lliang-jincheng@user.noreply.gitee.com>
2024-07-28 19:31:37 +0800

    feature/1010: 比赛队伍登出接口开发 --- TODO

--- gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/service/RegContestService.java
+++ gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/service/RegContestService.java
@@ -2,6 +2,7 @@ package com.gzhuoj.contest.service;
 
 import com.gzhuoj.contest.dto.req.RegContestGenTeamReqDTO;
 import com.gzhuoj.contest.dto.req.RegContestLoginReqDTO;
+import com.gzhuoj.contest.dto.req.RegContestLogoutReqDTO;
 import com.gzhuoj.contest.dto.resp.RegContestGenTeamRespDTO;
 
 import java.util.List;
@@ -10,4 +11,6 @@ public interface RegContestService {
     List<RegContestGenTeamRespDTO> genTeam(RegContestGenTeamReqDTO requestParam);
 
     void login(RegContestLoginReqDTO requestParam);
+
+    void logout(RegContestLogoutReqDTO requestParam);
 }
