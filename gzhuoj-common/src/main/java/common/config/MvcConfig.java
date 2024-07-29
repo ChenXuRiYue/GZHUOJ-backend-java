e0a9b6f6c33f894b516b6abf3519575ade90cb2f
Axel.Xun <chenjiuririyue@outlook.com>
2024-07-29 16:40:01 +0800

    feat: improve cors and login 联调前后端登录，完善了跨域问题的解决方案，引入一个统一的cors配置。完善token的传递逻辑。完成了login接口的前后端联调、对接

--- gzhuoj-common/src/main/java/common/config/MvcConfig.java
+++ gzhuoj-common/src/main/java/common/config/MvcConfig.java
@@ -2,11 +2,7 @@ package common.config;
 
 import common.interceptor.UserInfoInterceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,26 +13,5 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new UserInfoInterceptor());
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        //1. 添加 CORS配置信息
-        CorsConfiguration config = new CorsConfiguration();
-        //放行哪些原始域
-        config.addAllowedOriginPattern("*");
-        //是否发送 Cookie
-        config.setAllowCredentials(true);
-        //放行哪些请求方式
-        config.addAllowedMethod("*");
-        //放行哪些原始请求头部信息
-        config.addAllowedHeader("*");
-        //暴露哪些头部信息
-        config.addExposedHeader("*");
-        //2. 添加映射路径
-        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        corsConfigurationSource.registerCorsConfiguration("/**",config);
-        //3. 返回新的CorsFilter
-        return new CorsFilter(corsConfigurationSource);
     }
 }
