eeda48f88bd37ed9106fc02b5e141c85379dd555
sldpzshdwz <At19001214@outlook.com>
2024-08-13 17:09:23 +0800

    feature/1031:为插件提供滚榜所需要的数据（需要前端联调插件）

--- gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/mapper/ContestMapper.java
+++ gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/mapper/ContestMapper.java
@@ -2,9 +2,19 @@ package com.gzhuoj.contest.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.gzhuoj.contest.model.entity.ContestDO;
+import com.gzhuoj.contest.model.entity.SubmitDO;
+import com.gzhuoj.contest.model.entity.TeamDO;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 public interface ContestMapper extends BaseMapper<ContestDO> {
     @Select("SELECT * from contest where contest_id=#{contestId}")
     ContestDO selectByContestId(Integer contestId);
+
+    @Select("SELECT * from submit")
+    List<SubmitDO> sumbitSelectByContestId(Integer contestId);
+
+    @Select("SELECT * from team")
+    List<TeamDO> teamSelectByContestId(Integer contestId);
 }
