0b3f7ce529914a4b05ca47905ea252ad4e73ab89
sldpzshdwz <At19001214@outlook.com>
2024-08-13 17:09:23 +0800

    feature/1031:为插件提供滚榜所需要的数据（需要前端联调插件）

--- gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/service/contest/ContestService.java
+++ gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/service/contest/ContestService.java
@@ -8,6 +8,10 @@ import com.gzhuoj.contest.dto.req.contest.ContestStatusReqDTO;
 import com.gzhuoj.contest.dto.req.contest.ContestUpdateReqDTO;
 import com.gzhuoj.contest.dto.resp.contest.ContestAllRespDTO;
 import com.gzhuoj.contest.model.entity.ContestDO;
+import com.gzhuoj.contest.model.entity.SubmitDO;
+import com.gzhuoj.contest.model.entity.TeamDO;
+
+import java.util.List;
 
 public interface ContestService extends IService<ContestDO> {
     void update(ContestUpdateReqDTO requestParam);
@@ -19,4 +23,8 @@ public interface ContestService extends IService<ContestDO> {
     IPage<ContestAllRespDTO> all(ContestAllReqDTO requestParam);
 
     void changeStatus(ContestStatusReqDTO requestParam);
+
+    List<SubmitDO> sumbitData(Integer contestId);
+
+    List<TeamDO> teamData(Integer contestId);
 }
