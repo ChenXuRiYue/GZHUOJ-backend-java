8d98a2bd382b5cbae6c4be5cdd984eadd2d81713
sldpzshdwz <At19001214@outlook.com>
2024-08-15 10:26:53 +0800

    feature/1042:增加了考场分配座位的功能

---
+++ gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/model/pojo/PersonSeat.java
@@ -0,0 +1,11 @@
+package com.gzhuoj.contest.model.pojo;
+
+import lombok.Data;
+
+@Data
+public class PersonSeat {
+    String Name;
+    String examination;
+    Integer seat;
+    String teamName;
+}
