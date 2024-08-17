5f87f79e96445fb4716ab1f667ba5d7655eebba7
梁金成 <13893403+lliang-jincheng@user.noreply.gitee.com>
2024-08-17 16:29:05 +0800

    feature/1046: JudgeServer引入contest和problem远程调用接口

---
+++ gzhuoj-services/judge-server/src/main/java/com/gzhuoj/judgeserver/remote/DTO/ContestRemoteService.java
@@ -0,0 +1,34 @@
+package com.gzhuoj.judgeserver.remote.DTO;
+
+
+import com.gzhuoj.judgeserver.remote.DTO.req.SubmitRemoteDTO;
+import common.convention.result.Result;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.beans.Encoder;
+
+@FeignClient(name = "gzhuoj-contest-service",
+        configuration = {ContestRemoteService.MultipartSupportConfig.class})
+public interface ContestRemoteService {
+
+    @PutMapping("/api/gzhuoj-contest/submit")
+    Result<Boolean> submitUpdate(@RequestBody SubmitRemoteDTO requestParam);
+
+    @GetMapping("/api/gzhuoj-contest/code")
+    Result<String> getCode(@RequestParam("submitId") Integer submitId);
+    class MultipartSupportConfig {
+        @Bean
+        @Primary
+        @Scope("prototype")
+        public Encoder feignFormEncoder() {
+            return new Encoder();
+        }
+    }
+}
