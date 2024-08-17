b22f4295b82b78320741a5ff2777c0abf97e9a88
LJCzzzzZ <13893403+lliang-jincheng@user.noreply.gitee.com>
2024-08-17 16:33:06 +0800

    feature/1046: contest模块远程更新submit状态及获取提交源代码接口开发

--- gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/controller/JudgeController.java
+++ gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/controller/JudgeController.java
@@ -1,7 +1,12 @@
 package com.gzhuoj.contest.controller;
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.gzhuoj.contest.dto.req.Judge.RegContestJudgeSubmitReqDTO;
+import com.gzhuoj.contest.model.entity.SubmitDO;
+import com.gzhuoj.contest.remote.Req.SubmitRemoteDTO;
 import com.gzhuoj.contest.service.judge.JudgeService;
+import com.gzhuoj.contest.service.judge.SubmitService;
 import common.convention.result.Result;
 import common.convention.result.Results;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +20,7 @@ import static common.convention.errorcode.BaseErrorCode.JUDGE_SUBMIT_ERROR;
 @RequestMapping("/api/gzhuoj-contest")
 public class JudgeController {
     private final JudgeService judgeService;
+    private final SubmitService submitService;
     private final DiscoveryClient discoveryClient;
     /**
      * 提交代码 -- 链路起点
@@ -28,4 +34,16 @@ public class JudgeController {
         return Results.success();
     }
 
+    /**
+     * submitDO更新 用于远程调用
+     */
+    @PutMapping("/submit")
+    public Result<Boolean> submitUpdate(@RequestBody SubmitRemoteDTO requestParam){
+        return Results.success(submitService.updateSubmitDO(requestParam));
+    }
+
+    @GetMapping("/code")
+    public Result<String> getCode(Integer submitId){
+        return Results.success(submitService.getCode(submitId));
+    }
 }
--- gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/service/judge/SubmitService.java
+++ gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/service/judge/SubmitService.java
@@ -1,11 +1,21 @@
 package com.gzhuoj.contest.service.judge;
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.gzhuoj.contest.model.entity.SubmitDO;
+import com.gzhuoj.contest.remote.Req.SubmitRemoteDTO;
 
 public interface SubmitService extends IService<SubmitDO> {
     /**
      * 根据submitId 查询提交对象实体
      */
     SubmitDO getSubmitDO(Integer submitId);
+
+    /**
+     * 根据id更新 SubmitDO
+     */
+    boolean updateSubmitDO(SubmitRemoteDTO requestParam);
+
+    String getCode(Integer submitId);
 }
