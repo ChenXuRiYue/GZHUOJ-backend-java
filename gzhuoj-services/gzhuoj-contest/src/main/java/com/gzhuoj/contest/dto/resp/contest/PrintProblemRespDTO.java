8d247f3a88bf0fc338d6e7ae02e3339c2542b94f
sldpzshdwz <At19001214@outlook.com>
2024-08-19 16:01:16 +0800

    feature/1044

--- gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/dto/resp/contest/PrintProblemRespDTO.java
+++ gzhuoj-services/gzhuoj-contest/src/main/java/com/gzhuoj/contest/dto/resp/contest/PrintProblemRespDTO.java
@@ -1,7 +1,7 @@
 package com.gzhuoj.contest.dto.resp.contest;
 
 import com.gzhuoj.contest.model.entity.ContestDO;
-import common.model.pojo.ProblemPrint;
+import common.model.pojo.ProblemPrintDTO;
 import lombok.Data;
 
 import java.util.List;
@@ -10,5 +10,5 @@ import java.util.List;
 @Data
 public class PrintProblemRespDTO {
     ContestDO contest;
-    List<ProblemPrint> problems;
+    List<ProblemPrintDTO> problems;
 }
